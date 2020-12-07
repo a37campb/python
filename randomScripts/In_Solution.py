658e3571244df9b492f80bb5fbc94766a4149f7a
a37campb <aicamp4@hotmail.com>
2020-12-07 17:47:55 -0500

    lets try to make deepcode happy pt7

--- randomScripts/In_Solution.py
+++ randomScripts/In_Solution.py
@@ -1,8 +1,7 @@
 
 def firstMissingPositive(nums):
     if nums == []:
-        var = 1
-        return var
+        return 1
     lmt = max(nums)
     count = 0
     for _ in range(0,lmt+1):
@@ -13,3 +12,6 @@ def firstMissingPositive(nums):
             return count
 
 print(firstMissingPositive([0,1,2,3,-1,-10,-2,4,5,6,7,8,9,11]))
+
+
+# file deepcode ignore R1710: <ignore linter for return statements>
